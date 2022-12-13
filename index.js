55d85a3586d08654a13529fb4b628faf72080e58
Anthony Kaloferov <antony3620@gmail.com>
2022-12-13 14:21:50 +0200

    reuse module due to chrome

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { binaryEdgeDetection, color } from "./autocolor.js";
-
 // Selection of colors with which to randomly fill assumed coloring page
 var colors = [];
 var image;
@@ -145,4 +143,91 @@ function displayColors() {
     });
 
     pressme();
+}
+
+//////////////////////////////////////////////////////////////////////////
+////// autocolor.js but chrome CORS won't let me access it as module /////
+//////////////////////////////////////////////////////////////////////////
+
+// Get 'Value' in HSV color space from RGB image (used for better thresholding)
+function max(img) {
+    let out = new cv.Mat(img.rows, img.cols, cv.CV_8UC1);
+
+    for (let i = 0; i < img.rows; i++) {
+        for (let j = 0; j < img.cols; j++) {
+            let pixelImg = img.ucharPtr(i, j);
+            let R = pixelImg[0];
+            let G = pixelImg[1];
+            let B = pixelImg[2];
+
+            let m = Math.max(R, G, B);
+            let pixelMax = out.ucharPtr(i, j);
+            pixelMax[0] = m;
+        }
+    }
+
+    img.delete();
+
+    return out;
+}
+
+// Make all clearly black pixels based on threshold 0, else 255
+function threshold(img) {
+    cv.adaptiveThreshold(img, img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 43, 20)
+
+    return img;
+}
+
+// Stack 3 threshold matrices ontop to make a single 3 channel RGB placeholder image for filling in colors
+function stack(img) {
+    let out = new cv.Mat();
+    let planes = new cv.MatVector();
+
+    planes.push_back(img);
+    planes.push_back(img);
+    planes.push_back(img);
+
+    cv.merge(planes, out);
+
+    planes.delete();
+    img.delete();
+
+    return out;
+}
+
+// Convenience function executing preprocessing altogether
+function binaryEdgeDetection(img) {
+    return stack(threshold(max(img)));
+}
+
+// Use floodfill (paint-bucket tool) to fill in any large white connected structures in image
+function color(img, colors) {
+    // Move through the image, pixel by pixel, looking for a white pixel
+    function locateWhite(img, startpos) {
+        let pixel = img.ucharPtr(startpos[0], startpos[1]);
+
+        while (pixel[0] != 255 || pixel[0] != 255 || pixel[0] != 255) {
+            startpos[1] = (startpos[1] + 1) % img.cols
+            if (startpos[1] == 0) {
+                startpos[0] += 1;
+            }
+
+            if (startpos[0] == img.rows - 1 && startpos[1] == img.cols - 1) {
+                return null;
+            }
+
+            pixel = img.ucharPtr(startpos[0], startpos[1]);
+        }
+
+        return startpos;
+    };
+
+    let pos = locateWhite(img, [0, 0])
+    let mask = new cv.Mat.zeros(img.rows + 2, img.cols + 2, cv.CV_8UC1);
+    while (pos) {
+        cv.floodFill(img, mask, { x: pos[1], y: pos[0] }, colors[Math.floor(Math.random() * colors.length)]);
+        pos = locateWhite(img, pos);
+    }
+
+    return img;
 }
